4815313df71ba3538b9566606fa2ff1536793fb1
Donatas Bucinskas <donatas@tronikel.com>
2021-11-23 18:28:52 +0200

    rename

--- ios/covidMobileTests/covidMobileTests.m
+++ ios/covidMobileTests/covidMobileTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface covidMobileTests : XCTestCase
+@interface TCovidTests : XCTestCase
 
 @end
 
-@implementation covidMobileTests
+@implementation TCovidTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
